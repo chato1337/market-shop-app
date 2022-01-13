824d2cbabb28cb3a1d811325165b928543ded22b
samuelrs03 <74668615+didier-appspring@users.noreply.github.com>
2022-01-13 16:31:53 -0500

    add initial components & containers

---
+++ src/components/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import './Landing.styles.scss'
+
+const Landing = () => {
+    return (
+        <div className='landing-container'>
+            landing component
+        </div>
+    )
+}
+
+export default Landing
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            navbar component
+        </div>
+    )
+}
+
+export default Navbar
